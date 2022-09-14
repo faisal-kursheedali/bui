7f472aecd62c3bd7c01bb43c0f6df76d704d8fe2
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-09-14 21:24:35 +0530

    updated side nav

--- src/component/sideNav.jsx
+++ src/component/sideNav.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import "./css/sideNav.css"
 import {AiOutlineMenu} from "react-icons/ai"
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import { useData } from '../context/context'
 
 const SideNav = () => {
     const components=["avatar","alert","badge","button","image","input","card","list","toast","navigation","modal","rating","utility"]
     const {setSideNav}=useData();
+    const navigate=useNavigate();
     return (
         <div className="bui-sidenav doc-sidenav" >
             <div className="bui-sidenav-header">
                 <div className="bui-sidenav-header-btn" onClick={() => {
+
                     setSideNav(false);
                 }}>
                     <AiOutlineMenu />
                 </div>
                 <div className="bui-sidenav-header-logo" onClick={() => {
+                    // navigate("/")
                     setSideNav(false);
                 }}>
                 <Link to={"/"} className="bui-link-nostyle home-header-clr-txt">
@@ -24,16 +27,37 @@ const SideNav = () => {
                 </div>
             </div>
             <ul className="bui-sidenav-list">
-                <hr className="bui-sidenav-list-hr" />
+            
                 <li className="bui-sidenav-list-item bui-txt-weight-bold"  onClick={() => {
+                    navigate("/")
                     setSideNav(false);
                 }}>
-                    <Link to={"/doc/intro"} className="bui-link-nostyle bui-txt-weight-bold">Introduction</Link>
+                       Home
+                       
                 </li>
                 <li className="bui-sidenav-list-item bui-txt-weight-bold"  onClick={() => {
+                    navigate("/doc/intro")
                     setSideNav(false);
                 }}>
-                    <Link to={"/doc/installation"} className="bui-link-nostyle bui-txt-weight-bold">Installation</Link>
+                       Doc
+                       
+                </li>
+                <hr className="bui-sidenav-list-hr" />
+                <li className="bui-sidenav-list-item bui-txt-weight-bold"  onClick={() => {
+                    navigate("/doc/intro")
+                    setSideNav(false);
+                }}>
+                    {/* <Link to={"/doc/intro"} className="bui-link-nostyle bui-txt-weight-bold"> */}
+                        Introduction
+                        {/* </Link> */}
+                </li>
+                <li className="bui-sidenav-list-item bui-txt-weight-bold"  onClick={() => {
+                    navigate("/doc/installation")
+                    setSideNav(false);
+                }}>
+                    {/* <Link to={"/doc/installation"} className="bui-link-nostyle bui-txt-weight-bold"> */}
+                        Installation
+                        {/* </Link> */}
                 </li>
                 <hr className="bui-sidenav-list-hr" />
                 <li className="bui-sidenav-list-item bui-txt-weight-bold bui-txt-weight-bold">
@@ -43,10 +67,13 @@ const SideNav = () => {
                     components.map(i=>{
                         return(
 
-                    <li className="bui-sidenav-list-item" onClick={()=>setSideNav(false)} >
-                    <Link to={`/doc/${i}`} className='bui-link-nostyle' >
+                    <li className="bui-sidenav-list-item" style={{fontWeight:"initial"}} onClick={()=>{
+                        navigate(`/doc/${i}`)
+                        setSideNav(false)}
+                        } >
+                    {/* <Link to={`/doc/${i}`} className='bui-link-nostyle' > */}
                         {i}
-                    </Link>
+                    {/* </Link> */}
                     </li>
                         )
                     })
